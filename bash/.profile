7aca00d5a2a59670428c2d4c401c0895671d1c05
Jarne Clauw <jarne@clauw.eu>
2023-07-19 12:01:41 +0200

    [bash] adding another config and updating bashrc

---
+++ bash/.profile
@@ -0,0 +1,5 @@
+
+
+# Added by Toolbox App
+export PATH="$PATH:/home/jarne/.local/share/JetBrains/Toolbox/scripts"
+
--- bash/restore.sh
+++ bash/restore.sh
@@ -6,7 +6,7 @@ echo "Restoring bash configurations"
 repo_directory="$( cd -- "$(dirname "$0")/.." > /dev/null 2>&1 ; pwd -P )"
 
 # Remove the configurations
-rm "${HOME}/.bash_logout" "${HOME}/.bash_profile" "${HOME}/.bashrc" > /dev/null 2>&1
+rm "${HOME}/.bash_logout" "${HOME}/.bash_profile" "${HOME}/.profile" "${HOME}/.bashrc" > /dev/null 2>&1
 
 # Add the configurations back
 stow --ignore restore.sh --dir $repo_directory --target $HOME bash
