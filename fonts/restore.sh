0ee912efc337a2d3d6f71b83a39e3f3429d82ff1
Jarne Clauw <jarne@clauw.eu>
2023-07-22 10:29:38 +0200

    [fonts] Updating fonts restore script

--- fonts/restore.sh
+++ fonts/restore.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Restoring fonts"
-
 # Find the directory of the dotfiles repo
 repo_directory="$( cd -- "$(dirname "$0")/.." > /dev/null 2>&1 ; pwd -P )"
+
+# Echoing a descriptive message
+"${repo_directory}/echoMessage.sh" "Restoring fonts"
 
 # Make sure the path exists
 mkdir -p "${HOME}/.local/share/fonts/"
@@ -12,7 +13,7 @@ mkdir -p "${HOME}/.local/share/fonts/"
 rm -rf "${HOME}/.local/share/fonts/hack" > /dev/null 2>&1
 
 # Add the configurations back
-stow --ignore restore.sh --dir $repo_directory --target "${HOME}/.local/share/fonts" fonts
+stow --ignore "restore.sh" --dir "$repo_directory" --target "${HOME}/.local/share/fonts" fonts
 
 exit 0
 
