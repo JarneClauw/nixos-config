76e041f6d49bce56db978673e4084f17d4f08926
Jarne Clauw <jarne@clauw.eu>
2023-07-22 11:18:04 +0200

    [nvim] Adding configs

--- nvim/restore.sh
+++ nvim/restore.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Restoring Neovim configurations (AstroNvim)"
+# Find the dotfiles repository
+repo_directory="$( cd -- "$(dirname "$0")/.." > /dev/null 2>&1 ; pwd -P )"
+
+# Echoing a descriptive message
+"${repo_directory}/echoMessage.sh" "Restoring Neovim configurations (based on AstroNvim)"
+
+# Install neovim
+"${repo_directory}/installDnf.sh" "neovim"
 
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
