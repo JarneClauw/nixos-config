1ac4a0e99c8662f0a73f3a1b113686ea7f97c297
Jarne Clauw <jarne@clauw.eu>
2023-07-22 10:34:30 +0200

    [git] Updating restore script

--- git/restore.sh
+++ git/restore.sh
@@ -6,11 +6,14 @@ repo_directory="$( cd -- "$(dirname "$0")/.." > /dev/null 2>&1 ; pwd -P )"
 # Adding a descriptive message
 "${repo_directory}/echoMessage.sh" "Restoring git configurations"
 
+# Installing git (probably already installed)
+"${repo_directory}/installDnf.sh" "git"
+
 # Removing old configurations
 rm "${HOME}/.gitconfig" > /dev/null 2>&1
 
 # Add the configurations back
-stow --ignore restore.sh --dir $repo_directory --target $HOME git
+stow --ignore "restore.sh" --dir "$repo_directory" --target "$HOME" git
 
 exit 0
 
