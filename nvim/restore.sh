5005ab3a09a68b1f2323ba0d8cf06ba72fa2c1e0
Jarne Clauw <jarne@clauw.eu>
2023-07-22 11:25:31 +0200

    [nvim] Updating restore script

--- nvim/restore.sh
+++ nvim/restore.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Original: https://github.com/AstroNvim/AstroNvim
+
 # Find the dotfiles repository
 repo_directory="$( cd -- "$(dirname "$0")/.." > /dev/null 2>&1 ; pwd -P )"
 
@@ -9,7 +11,14 @@ repo_directory="$( cd -- "$(dirname "$0")/.." > /dev/null 2>&1 ; pwd -P )"
 # Install neovim
 "${repo_directory}/installDnf.sh" "neovim"
 
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+# Check if the directories exist
+mkdir -p "${HOME}/.config/nvim"
+
+# Remove old configurations
+rm -rf "${HOME}/.config/nvim/*" > /dev/null 2>&1
+
+# Adding my configurations back
+stow --ignore "restore.sh" --dir "$repo_directory" --target "${HOME}/.config/nvim/" nvim
 
 exit 0
 
